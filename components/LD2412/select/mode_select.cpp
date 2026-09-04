c264cd3cb22344cc02de4fa764cd1e34139f2d32
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-09-04 03:25:49 +0100

    Update mode_select.cpp

--- components/LD2412/select/mode_select.cpp
+++ components/LD2412/select/mode_select.cpp
@@ -5,7 +5,7 @@ namespace LD2412 {
 
 void ModeSelect::control(const std::string &value) {
   this->publish_state(value);
-  this->parent_->set_mode(state);
+  this->parent_->set_mode(value);
 }
 
 }  // namespace LD2412
