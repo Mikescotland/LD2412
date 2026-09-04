f487b810e4afcd2057b95c4b62a9cb662d3ff112
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-09-04 03:24:11 +0100

    Update baud_rate_select.cpp

--- components/LD2412/select/baud_rate_select.cpp
+++ components/LD2412/select/baud_rate_select.cpp
@@ -5,7 +5,7 @@ namespace LD2412 {
 
 void BaudRateSelect::control(const std::string &value) {
   this->publish_state(value);
-  this->parent_->set_baud_rate(state);
+  this->parent_->set_baud_rate(value);
 }
 
 }  // namespace LD2412
